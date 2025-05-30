342d4d593f6d65a7f1026232930f7395132cedc7
Ratan Kumar <krratan80@gmail.com>
2025-05-30 09:29:53 +0530

    correction in navbar

--- app.py
+++ app.py
@@ -6,7 +6,16 @@ app = Flask(__name__)
 # Home Page
 @app.route('/')
 def index():
-    return render_template('home.html', title='Home | Bright Career Path')
+    services = [
+        {'title': 'Digital Marketing', 'description': 'Odio excepturi sequi at ullam fugiat iusto nemo debitis nostrum eveniet soluta beatae, perferendis doloremque sapiente molestiae iure eum incidunt? Minus, nobis.', 'image': 'images/image-1.jpg'},
+        {'title': 'Digital Marketing', 'description': 'Odio excepturi sequi at ullam fugiat iusto nemo debitis nostrum eveniet soluta beatae, perferendis doloremque sapiente molestiae iure eum incidunt? Minus, nobis.', 'image': 'images/image-1.jpg'},
+        {'title': 'Digital Marketing', 'description': 'Odio excepturi sequi at ullam fugiat iusto nemo debitis nostrum eveniet soluta beatae, perferendis doloremque sapiente molestiae iure eum incidunt? Minus, nobis.', 'image': 'images/image-1.jpg'},
+        {'title': 'Digital Marketing', 'description': 'Odio excepturi sequi at ullam fugiat iusto nemo debitis nostrum eveniet soluta beatae, perferendis doloremque sapiente molestiae iure eum incidunt? Minus, nobis.', 'image': 'images/image-1.jpg'},
+        {'title': 'Digital Marketing', 'description': 'Odio excepturi sequi at ullam fugiat iusto nemo debitis nostrum eveniet soluta beatae, perferendis doloremque sapiente molestiae iure eum incidunt? Minus, nobis.', 'image': 'images/image-1.jpg'},
+        {'title': 'Digital Marketing', 'description': 'Odio excepturi sequi at ullam fugiat iusto nemo debitis nostrum eveniet soluta beatae, perferendis doloremque sapiente molestiae iure eum incidunt? Minus, nobis.', 'image': 'images/image-1.jpg'},
+        
+    ]
+    return render_template('home.html', title='Home | Bright Career Path', services=services)
     
 if __name__ == '__main__':
     app.run(debug=True)
